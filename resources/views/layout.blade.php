16dcc49dbac45ef839fac8f0a1be5b07dc0233d5
maaimahmoud <mai.kenawy97@eng-st.cu.edu.eg>
2018-03-06 15:10:30 +0200

    Homepage and nav bar modification

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,12 +10,40 @@
 
         <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" >
 
+        <style type="text/css">
+            :root {
+                --main-color: #bbbbbb;
+                }
+              .navbar{
+                background-color: var(--main-color);
+                margin-bottom: 20px;
+              }
+              .btn{
+                background-color: var(--main-color);
+              }
+              .footer-copyright{
+                background-color: var(--main-color);
+                margin-top: 20px;
+              }
+              #pageTitle{
+                position: absolute;
+                top: 20%;
+                left: 45%;
+                text-align: center;
+                font-weight: bold;
+                font-family: "Lucida Console", Monaco, monospace;
+              }
+
+        </style>
+
         <title>{{ config('app.name','Company') }}</title>
   </head>
-      <nav class="navbar navbar-light" style="background-color: #eef00d;">
+      <nav class="navbar navbar-light" style="height:90px">
               <!-- Navbar content -->
-                <h2>Renewal System</h2>
+              <h3 id="pageTitle">@yield('title')</h3>
       </nav>
+      <a href="/"><img src="images/company-logo.png" alt="Company logo" style=" max-width:130px; position: absolute; top: 15px; left: 50px;"></a>
+
       <div class="container-fluid">
             @yield('content')
       </div>
@@ -29,4 +57,25 @@
       <script src="{{ asset('js/jquery-3.3.1.min.js') }}" > </script>
       <script src="{{ asset('js/popper.min.js') }}" > </script>
       <script src="{{ asset('js/bootstrap.min.js') }}" > </script>
+      <script>
+      $(".full-height").css({
+              "min-height": $(window).height() - $(".footer-copyright").innerHeight() - $("nav").innerHeight()
+          });
+          $(window).on('resize', function() {
+              $(".full-height").css({
+                "min-height": $(window).height() - $(".footer-copyright").innerHeight() - $("nav").innerHeight()
+          });
+      });
+
+      $(document).ready(function () {
+      //your code here
+      $('#html5colorpicker').change(function() {
+              console.log('hi i was called');
+              var newcolor = $('#html5colorpicker').val();
+              //element.style.setProperty("--main-color", color);
+              //document.body.style.setProperty('var(--main-color)', #000000);//set
+              document.body.style.setProperty("--main-color", newcolor);
+        });
+      });
+      </script>
 </html>
