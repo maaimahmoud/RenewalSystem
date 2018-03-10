5f6a35a0437fd9a66e2197ea2062e985bf3d5ec3
abdokaseb <abdokaseb@gmail.com>
2018-03-10 12:57:43 +0200

    Edit client service seeder add the date time for update and create for good checks

--- database/factories/ClientServiceFactory.php
+++ database/factories/ClientServiceFactory.php
@@ -8,7 +8,10 @@ $factory->define(App\ClientService::class, function (Faker $faker) {
         'service_id'=>$faker->numberBetween(1, App\Service::count()),
         'payment_method'=>$faker->numberBetween(1, App\PaymentMethod::count()),
         'balance'=>$faker->numberBetween(-100,1000),
-        'end_time'=>$faker->dateTime,
+        'end_time'=>$faker->dateTimeInInterval($startDate = '-1 years', $interval = '+ 5 years', $timezone = null) ,
         'required_money'=>$faker->numberBetween(1,1000),
+        'updated_at'=>$faker->dateTimeInInterval($startDate = '-45 days', $interval = '+ 40 days', $timezone = null) ,
+        'created_at'=>$faker->dateTimeInInterval($startDate = '-1 years', $interval = '+ 1 years', $timezone = null) ,
+
     ];
 });
