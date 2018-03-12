85c804c9120d4f09d2fdd2a0acd4fca43f80bd13
abdokaseb <abdokaseb@gmail.com>
2018-03-12 19:24:47 +0200

    edit seeders after database edits

--- database/factories/MailingClientServiceFactory.php
+++ database/factories/MailingClientServiceFactory.php
@@ -4,7 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\MailingMethodClinetServices::class, function (Faker $faker) {
     return [
-        'mailing_methods_id'=>$faker->numberBetween(1, App\MailingMethod::count()),
+        'days_to_mail'=>$faker->numberBetween(1, 45),
+        'required_days_to_pay'=>$faker->numberBetween(1, 400),
+        'last_paid_date'=>$faker->dateTimeInInterval($startDate = '-1 years', $interval = '+ 1 years', $timezone = null) ,
         'client_services_id'=>$faker->numberBetween(1, App\ClientService::count()),
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,11 +14,11 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         factory(App\User::class,10)->create();
         factory(App\ServiceCategories::class,10)->create();
-        factory(App\PaymentMethod::class,10)->create();
-        factory(App\MailingMethod::class,10)->create();
-        factory(App\Client::class,100)->create();
-        factory(App\Service::class,100)->create();
-        factory(App\ClientService::class,300)->create();
-        factory(App\MailingMethodClinetServices::class,1000)->create();
+        factory(App\PaymentMethod::class,20)->create();
+        factory(App\MailingMethod::class,20)->create();
+        factory(App\Client::class,1000)->create();
+        factory(App\Service::class,1000)->create();
+        factory(App\ClientService::class,3000)->create();
+        factory(App\MailingMethodClinetServices::class,10000)->create();
     }
 }
