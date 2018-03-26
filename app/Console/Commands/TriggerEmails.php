23486ffb6dc6987b75cf8a0471f325028b47ee68
abdokaseb <abdokaseb@gmail.com>
2018-03-26 06:37:46 +0200

    last commit in mailing system ISA

--- app/Console/Commands/TriggerEmails.php
+++ app/Console/Commands/TriggerEmails.php
@@ -43,15 +43,8 @@ class TriggerEmails extends Command
      */
     public function handle()
     {
-
-        $user = new User;
-        $user->name="sddsfsdf";
-        $user->password =str_random(40);
-        $user->email = str_random(40);
-        $user->save();
-
-
         //finished services should be deleted from table mails
+        //get clients ids from the database
         $client_services_id_to_mail = $this->getClientsToMail ();
 
         if ($client_services_id_to_mail)
@@ -82,26 +75,21 @@ class TriggerEmails extends Command
                 ->whereIn('client_services.id', $ids)
                 ->join('clients', 'client_services.client_id', '=', 'clients.id')
                 ->join('services', 'client_services.service_id', '=', 'services.id')
-                ->select('clients.name as client_name', 'clients.email','clients.phone_number','client_services.balance','client_services.required_money','services.title as service_name','services.email_template')
+                ->select('clients.name as client_name', 'clients.email as email','clients.phone_number','client_services.balance','client_services.required_money','services.title as service_name','services.email_template')
                 ->get();
 
     }
 
     protected function sendMails ($mails_info)
     {
-        $late_time = 10;
-        $send_time = now()->addMinutes($late_time);
-
         foreach ($mails_info as $data){
             $mail_info=((array) $data);
             //sending mail to every client
             Mail::send('mail', $mail_info, function($message) use ($mail_info) {
-                $message->to("abdokaseb@gmail.com", $mail_info['client_name'])
+                $message->to($mail_info['email'], $mail_info['client_name'])
                         ->subject($mail_info['service_name']);
-                $message->from('systemrenewal@gmail.com','Renewal System');//->later($when);
+                $message->from('systemrenewal@gmail.com','Renewal System');
             });
-        
-            $send_time=$send_time->addMinutes($late_time);
         }
       
     }
