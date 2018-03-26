b1d3adebac84261ab9f59359d8131fa2f51fb3da
abdokaseb <abdokaseb@gmail.com>
2018-03-26 05:58:50 +0200

    it's just work now ... sending fully automatic mails waiting for delay between mails on same day

--- app/Console/Commands/TriggerEmails.php
+++ app/Console/Commands/TriggerEmails.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Str;
 use DB;
 use Mail;
 
-define("late_time",60);
 
 class TriggerEmails extends Command
 {
@@ -44,47 +43,65 @@ class TriggerEmails extends Command
      */
     public function handle()
     {
+
+        $user = new User;
+        $user->name="sddsfsdf";
+        $user->password =str_random(40);
+        $user->email = str_random(40);
+        $user->save();
+
+
         //finished services should be deleted from table mails
-        $client_services_id_to_mail = getClientsToMail ();
+        $client_services_id_to_mail = $this->getClientsToMail ();
+
         if ($client_services_id_to_mail)
         {
-            $Mails_Infos = getMailsInfo($client_services_id_to_mail);
-            sendMails ($Mails_Infos);
+            $Mails_Infos = $this->getMailsInfo($client_services_id_to_mail);
+            $this->sendMails ($Mails_Infos);
         }
+        $this->info('The happy birthday messages were sent successfully!');
 
     }
 
     protected function getClientsToMail ()
     {
-        return DB::select('SELECT client_services_id
+        $result = DB::select('SELECT client_services_id as id
                             FROM mailing_method_clinet_services
                             WHERE  DATE_ADD( last_paid_date , INTERVAL required_months_to_pay MONTH) > curdate()
                             AND DATEDIFF( DATE_ADD( last_paid_date , INTERVAL required_months_to_pay MONTH), curdate() ) = days_to_mail;');
-
+        $ID_Arr = [];
+        foreach($result as $row){
+            $ID_Arr[] = ((array) $row)['id'];
+        }
+        return $ID_Arr;        
     }
 
     protected function getMailsInfo ($ids)
     {
         return DB::table('client_services')
-                ->whereIn('id', $ids)
+                ->whereIn('client_services.id', $ids)
                 ->join('clients', 'client_services.client_id', '=', 'clients.id')
                 ->join('services', 'client_services.service_id', '=', 'services.id')
-                ->select('clients.name as client_name', 'clients.email','clients.phone_number','client_services.balance','client_services.requierd_money','services.title as service_name','services.email_template')
+                ->select('clients.name as client_name', 'clients.email','clients.phone_number','client_services.balance','client_services.required_money','services.title as service_name','services.email_template')
                 ->get();
 
     }
 
     protected function sendMails ($mails_info)
     {
+        $late_time = 10;
         $send_time = now()->addMinutes($late_time);
 
-        foreach ($mails_info as $mail_info){
-            Mail::send('mail', $mail_info, function($message) {
+        foreach ($mails_info as $data){
+            $mail_info=((array) $data);
+            //sending mail to every client
+            Mail::send('mail', $mail_info, function($message) use ($mail_info) {
                 $message->to("abdokaseb@gmail.com", $mail_info['client_name'])
                         ->subject($mail_info['service_name']);
                 $message->from('systemrenewal@gmail.com','Renewal System');//->later($when);
-                });
-            $send_time=$send_time+$late_time;
+            });
+        
+            $send_time=$send_time->addMinutes($late_time);
         }
       
     }
--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,2 +1,2 @@
-<h1>Hi, {{ $name }}</h1>
+<h1>Hi</h1>
 <p>Sending Mail from Laravel.</p>
