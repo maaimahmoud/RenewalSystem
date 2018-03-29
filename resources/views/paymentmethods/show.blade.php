c2bad202b5621d27d57009bcd44443d69e4134da
reemashraf <remoash97@gmail.com>
2018-03-29 18:09:38 +0200

    fixing adding and editing paymentmethods/servicescategories

--- resources/views/paymentmethods/show.blade.php
+++ resources/views/paymentmethods/show.blade.php
@@ -28,9 +28,6 @@
     </div>
   </div>
   @endforeach
-       @else
-<div class="alert alert-danger" role="alert"> there is no PaymentMethods </div>
-@endif
 </div>
 @foreach ($paymentmethods as $payment)
      <div class="modal fade" id="deleteModalCenter{{ $payment->id }}" tabindex="-1" role="dialog" aria-labelledby="deleteModalCenterTitle" aria-hidden="true">
@@ -46,15 +43,12 @@
             Are you sure you want to delete this payment Method ?
           </div>
           <div class="modal-footer">
-            <a type="button" class= "btn-outline-primary" data-dismiss="modal">Cancel</a>
             <a type="button" class="btn btn-outline-primary" href="{{url('/paymentmethods/delete/'.$payment->id)}}">Delete</a>
+             <a type="button" class="btn btn-outline-primary" data-dismiss="modal">Cancel</a>
           </div>
         </div>
-
-          </div>
-        </div>
-
-
+      </div>
+   </div>
 @endforeach
             @foreach ($paymentmethods as $payment)
                 <div class="modal fade" id="editModalCenter{{ $payment->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
@@ -67,7 +61,7 @@
                               </button>
                             </div>
                             <div class="modal-body mb-0">
-                              <form action="{{url('/paymentmethods/edit/'.$payment->id)}}" method="POST">
+                              <form action="{{route('paymentmethods.update', $payment->id)}}" method="POST">
                                     @csrf
                                     {{ method_field('PUT') }}
                                         <div class="md-form form-sm" >
@@ -81,8 +75,8 @@
                                             <input type="number" class="form-control" name="months" min=1  value={{ $payment->months}} required>
                                         </div>
                                         <div class="text-center mt-1-half">
-                                            <button type ="submit" class="btn btn-secondary " ><i class="fa fa-send ml-1"></i>Edit</button>
-                                            <a type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</a>
+                                             <button type="submit" class="btn btn-outline-primary"><i class="fa fa-send ml-1"></i>Edit</button>
+                                            <a type="button" class="btn btn-outline-primary" data-dismiss="modal">Cancel</a>
                                         </div>
                                 </form>
                             </div>
@@ -90,4 +84,42 @@
                     </div>
                 </div>
             @endforeach
+
+<div class="modal fade" id="addmodalpaymentmethod" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                          <div class="modal-header light-blue darken-3 white-text">
+                              <h5 class="modal-title" id="editModalLongTitle">Edit</h5>
+                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                              </button>
+                            </div>
+                            <div class="modal-body mb-0">
+                              <form action="{{route('paymentmethods.store')}}" method="POST">
+                                    @csrf
+                                    {{ method_field('POST') }}
+                                        <div class="md-form form-sm" >
+                                            <i class="fa fa-tag prefix"></i>
+                                             <label for="form21">Title</label>
+                                            <input type="text" class="form-control" name="title"  required>
+                                        </div>
+                                        <div class="md-form form-sm" >
+                                            <i class="fa fa-tag prefix"></i>
+                                             <label for="form21">Number of months</label>
+                                            <input type="number" class="form-control" name="months" min=1  required>
+                                        </div>
+                                        <div class="text-center mt-1-half">
+                                            <button type ="submit" class="btn btn-outline-primary" ><i class="fa fa-send ml-1"></i>
+                                            add</button>
+                                            <a type="button" class="btn btn-outline-primary"data-dismiss="modal">Cancel</a>
+                                        </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+ @else
+<div class="alert alert-danger" role="alert"> there is no PaymentMethods </div>
+@endif
 @endsection
