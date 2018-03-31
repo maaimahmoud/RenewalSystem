d94a334b463db9ece4cbfa95e01f8f95ca8a54ef
abdokaseb <abdokaseb@gmail.com>
2018-03-31 19:58:15 +0200

    add to controller and view for statistics

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -3,8 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\ServiceCategories;
+use App\PaymentMethod;
+use App\Client;
+use App\Service;
+use App\ClientService;
 
 class StatisticsController extends Controller
 {
-    //
+    public function index()
+    {   
+        $user_time = Charts::database(User::all(), 'bar', 'highcharts')
+                    ->elementLabel("Total")
+                    ->dimensions(1000, 500)
+                    ->responsive(true)
+                    ->groupByYear(5);
+        $service_categories_time = Charts::database(ServiceCategories::all(), 'bar', 'highcharts')
+                    ->elementLabel("Total")
+                    ->dimensions(1000, 500)
+                    ->responsive(true)
+                    ->groupByYear(5);
+        $client_time = Charts::database(Client::all(), 'bar', 'highcharts')
+                    ->elementLabel("Total")
+                    ->dimensions(1000, 500)
+                    ->responsive(true)
+                    ->groupByYear(5);
+        $service_time = Charts::database(Service::all(), 'bar', 'highcharts')
+                    ->elementLabel("Total")
+                    ->dimensions(1000, 500)
+                    ->responsive(true)
+                    ->groupByYear(5);
+        $service_time = Charts::database(Service::all(), 'bar', 'highcharts')
+                    ->elementLabel("Total")
+                    ->dimensions(1000, 500)
+                    ->responsive(true)
+                    ->groupByYear(5);
+        
+        $chart = $service_time;
+        return view('test', ['chart' => $chart]);
+
+
+    }
 }
--- resources/views/statistics.blade.php
+++ resources/views/statistics.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2 style="text-align: center; margin-top:15%;">This page is not avaliable yet.</h2>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>My Charts</title>
+
+        {!! Charts::styles() !!}
+
+    </head>
+    <body>
+        <!-- Main Application (Can be VueJS or other JS framework) -->
+        <div class="app">
+            <center>
+                {!! $chart->html() !!}
+            </center>
+        </div>
+        <!-- End Of Main Application -->
+        {!! Charts::scripts() !!}
+        {!! $chart->script() !!}
+    </body>
+</html>
 @endsection
