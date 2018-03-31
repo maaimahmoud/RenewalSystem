a6db16fdee190564f5eefd1393f2efdd01cbff5b
reemashraf <remoash97@gmail.com>
2018-03-31 20:22:39 +0200

    starting front end of charts

--- resources/views/statistics.blade.php
+++ resources/views/statistics.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 
+
 @section('content')
-    <h2 style="text-align: center; margin-top:15%;">This page is not avaliable yet.</h2>
+<div class="row">
+  <div class="card bg-danger text-white ml-5 mr-5" style="width: 15rem">
+    <div class="card-body">Primary card</div>
+  </div>
+  <div class="card bg-warning text-white mr-5" style="width: 15rem">
+    <div class="card-body">Primary card</div>
+  </div>
+  <div class="card bg-info text-white mr-5" style="width: 15rem">
+    <div class="card-body">Primary card</div>
+  </div>
+  <div class="card bg-success text-white mr-5" style="width: 15rem">
+    <div class="card-body">Primary card</div>
+  </div>
+  </div>
 @endsection
