264d4d41b3a601dd501380dd852b81f6bffd97e0
abdokaseb <abdokaseb@gmail.com>
2018-03-31 19:13:08 +0200

    add created_at and updated_at to factories

--- database/factories/PaymentFactory.php
+++ database/factories/PaymentFactory.php
@@ -6,5 +6,7 @@ $factory->define(App\PaymentMethod::class, function (Faker $faker) {
     return [
         'title' => $faker->unique()->text($maxNbChars = 20),
         'months' => $faker->unique()->numberBetween(1,24),
+        'created_at'=>$faker->dateTimeInInterval($startDate = '-6 years', $interval = '+ 6 years', $timezone = null) ,
+        'updated_at'=>$faker->dateTimeInInterval($startDate = '-6 years', $interval = '+ 6 years', $timezone = null) 
     ];
 });
