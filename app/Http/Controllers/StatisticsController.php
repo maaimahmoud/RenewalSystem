1cb31e104cf110b54f130ca454cb9cb9d0adf69f
reemashraf <remoash97@gmail.com>
2018-04-01 12:56:45 +0200

    finishing 1st part of statistics front and end

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -14,34 +14,32 @@ class StatisticsController extends Controller
 {
     public function index()
     {   
-        $user_time = Charts::database(User::all(), 'bar', 'highcharts')
-                    ->elementLabel("Total")
-                    ->dimensions(1000, 500)
-                    ->responsive(true)
-                    ->groupByYear(5);
+        $service_count=(Service::all()->count());
+        $client_count=(Client::all()->count());
+        $service_categories_count=(ServiceCategories::all()->count());
+        $payment_method_count=(PaymentMethod::all()->count());
+
         $service_categories_time = Charts::database(ServiceCategories::all(), 'bar', 'highcharts')
-                    ->elementLabel("Total")
+                    ->elementLabel("Categories")
+                    ->title('Service_Categories')
                     ->dimensions(1000, 500)
                     ->responsive(true)
                     ->groupByYear(5);
         $client_time = Charts::database(Client::all(), 'bar', 'highcharts')
-                    ->elementLabel("Total")
+                    ->elementLabel("Clients")
+                    ->title('Clients')
                     ->dimensions(1000, 500)
                     ->responsive(true)
                     ->groupByYear(5);
         $service_time = Charts::database(Service::all(), 'bar', 'highcharts')
-                    ->elementLabel("Total")
+                    ->elementLabel("Services")
+                    ->title('Services')
                     ->dimensions(1000, 500)
                     ->responsive(true)
                     ->groupByYear(5);
-        $service_time = Charts::database(Service::all(), 'bar', 'highcharts')
-                    ->elementLabel("Total")
-                    ->dimensions(1000, 500)
-                    ->responsive(true)
-                    ->groupByYear(5);
+       
         
-        $chart = $service_time;
-        return view('statistics', compact('chart'));
+        return view('statistics', compact('service_time','client_time','service_categories_time','service_count','client_count','service_categories_count','payment_method_count'));
 
 
     }
