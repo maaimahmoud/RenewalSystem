0fd509e93af4eb65e3987ff11af4070545a56d4b
maaimahmoud <mai.kenawy97@eng-st.cu.edu.eg>
2018-05-06 02:44:41 +0200

    merging

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -11,6 +11,7 @@ use App\Service;
 use App\ClientService;
 use Charts;
 use DB;
+use Faker\Factory as Faker;
 
 class StatisticsController extends Controller
 {
@@ -25,16 +26,149 @@ class StatisticsController extends Controller
         $this->middleware('auth');
     }
 
+    public function getcolors($number)
+    {
+$colors =array ("FFA07A",
+"20B2AA",
+"87CEFA",
+"778899",
+"FFDEAD",
+"4682B4",
+"FFFFE0",
+"00FF00",
+"32CD32",
+"7FFFD4",
+"F0FFFF",
+"F5F5DC",
+"FFE4C4",
+"FFEBCD",
+"0000FF",
+"8A2BE2",
+"A52A2A",
+"DEB887",
+"5F9EA0",
+"7FFF00",
+"FFFAFA",
+"00FF7F",
+"FFD700",
+"D2B48C",
+"008080",
+"D8BFD8",
+"FF6347",
+"40E0D0",
+"778899",
+"B0C4DE",
+"D2691E",
+"FF7F50",
+"6495ED",
+"FFF8DC",
+"DC143C",
+"00FFFF",
+"00008B",
+"008B8B",
+"B8860B",
+"A9A9A9",
+"006400",
+"FAF0E6",
+"FF00FF",
+"800000",
+"66CDAA",
+"0000CD",
+"BA55D3",
+"9370DB",
+"3CB371",
+"7B68EE",
+"FFFF00",
+"00FA9A",
+"48D1CC",
+"C71585",
+"191970",
+"F5FFFA",
+"FFE4E1",
+"000080",
+"FDF5E6",
+"808000",
+"6B8E23",
+"FFA500",
+"FF4500",
+"DA70D6",
+"EEE8AA",
+"98FB98",
+"FFC0CB",
+"AFEEEE",
+"DB7093",
+"FFEFD5",
+"FFDAB9",
+"CD853F",
+"DDA0DD",
+"B0E0E6",
+"800080",
+"663399",
+"FF0000",
+"BC8F8F",
+"4169E1",
+"8B4513",
+"FA8072",
+"F4A460",
+"2E8B57",
+"FFF5EE",
+"A0522D",
+"C0C0C0",
+"FFE4B5",
+"87CEEB",
+"6A5ACD",
+"708090",
+"708090",
+"EE82EE",
+"F5DEB3",
+"F5F5F5",
+
+);
+        for ($i=0;$i<$number;$i++)
+        {
+            $i=$i%93;
+            $colors[$i]='#'.$colors[$i];
+        }
+       /* $faker=\Faker\Factory::create('en_US');
+        for ($i=0;$i<$number;$i++)
+        {
+            $colors[$i]=$faker->unique()->numberBetween(1,16700000);
+            $colors[$i]='#'.$colors[$i];
+        }*/
+/*
+    for ($i=0;$i<$number;$i++)
+    {
+    $intnumber=rand(5,10);
+    $red=$intnumber*rand(0,255);
+    $green =$intnumber*rand(0,255);
+    $blue = $intnumber*rand(0,255);
+
+    $red = ($intnumber&0x0000ff) << 16;
+    $green = ($intnumber&0x00ff00) <<8;
+    $blue = ($intnumber&0xff0000) >> 10;
+
+    $intnumber = $red|$green|$blue;
+      // toString converts a number to a hexstring
+      $HTMLcolor = (string)$intnumber;
+      // adding # before each color
+      $colors[$i]= '#' . $HTMLcolor;
+  }
+for ($i=0;$i<$colors->count();$i++)
+{
+    if ($colors[i])
+}*/
+    return ($colors);
+}
 
     public function index()
     {   
+         //return (StatisticsController::getcolors(25));
         $service_count=(Service::all()->count());
         $client_count=(Client::all()->count());
         $service_categories_count=(ServiceCategories::all()->count());
         $payment_method_count=(PaymentMethod::all()->count());
 
-        $service_categories_time = StatisticsController::getServiceChartWithTime();
-
+        $service_categories_time = StatisticsController::getServiceCategoryChartWithTime();
 
         $client_time =StatisticsController::getCientChartWithTime();
 
@@ -56,18 +190,20 @@ public function getClientServiceQuery()
                     ->join('services','service_id','=','services.id')
                     ->groupBy('service_id','services.title')
                     ->get();
-        return($clients_by_services_query);
+        $count=$clients_by_services_query->count();
+        return compact('clients_by_services_query','count');
 }
 public function getClientServiceChart()
 {
-    $clients_by_services_query=StatisticsController::getClientServiceQuery();
+    $clients_by_services_q=StatisticsController::getClientServiceQuery();
       $clients_by_services = Charts::create('pie', 'highcharts')
+                    ->colors(StatisticsController::getcolors($clients_by_services_q['count']))
                     ->title('clients_by_services')
                     ->elementLabel('clients_by_services')
-                    ->labels($clients_by_services_query->pluck('title'))
-                    ->values($clients_by_services_query->pluck('num'))
-                     ->width(1200)
-                     ->height(700)
+                    ->labels($clients_by_services_q['clients_by_services_query']->pluck('title'))
+                    ->values($clients_by_services_q['clients_by_services_query']->pluck('num'))
+                    ->width(1200)
+                    ->height(700)
                     ->responsive(false);
 
     return ($clients_by_services);
@@ -84,16 +220,18 @@ public function getClientCategoryQuery()
                     ->join('service_categories','services.category_id','=','service_categories.id')
                     ->groupBy('service_categories.id','service_categories.title')
                     ->get();
-    return($clients_by_categories_query);
+       $count=$clients_by_categories_query->count();
+    return compact('clients_by_categories_query','count');
 }
 public function getClientCategoryChart()
 {
-    $clients_by_categories_query =StatisticsController::getClientCategoryQuery();
+    $clients_by_categories_q =StatisticsController::getClientCategoryQuery();
     $clients_by_categories = Charts::create('pie', 'highcharts')
                     ->title('clients_by_categories')
+                    ->colors(StatisticsController::getcolors($clients_by_categories_q['count']))
                     ->elementLabel('clients_by_categories')
-                    ->labels($clients_by_categories_query->pluck('title'))
-                    ->values($clients_by_categories_query->pluck('num'))
+                    ->labels($clients_by_categories_q['clients_by_categories_query']->pluck('title'))
+                    ->values($clients_by_categories_q['clients_by_categories_query']->pluck('num'))
                      ->width(1200)
                      ->height(700)
                     ->responsive(false);
@@ -107,16 +245,18 @@ public  function getClientPaymentMethodQuery()
                     ->join('payment_methods','payment_method','=','payment_methods.id')
                     ->groupBy('payment_method','payment_methods.title')
                     ->get();
-        return($clients_by_payment_methods_query);
+        $count=$clients_by_payment_methods_query->count();
+        return compact('clients_by_payment_methods_query','count');
 }
 public  function getClientPaymentMethodChart()
 {
-    $clients_by_payment_methods_query=StatisticsController::getClientPaymentMethodQuery();
+    $clients_by_payment_methods_q=StatisticsController::getClientPaymentMethodQuery();
       $clients_by_payment_methods = Charts::create('pie', 'highcharts')
                     ->title('clients_by_payment_methods')
+                    ->colors(StatisticsController::getcolors($clients_by_payment_methods_q['count']))
                     ->elementLabel('clients_by_payment_methods')
-                    ->labels($clients_by_payment_methods_query->pluck('title'))
-                    ->values($clients_by_payment_methods_query->pluck('num'))
+                    ->labels($clients_by_payment_methods_q['clients_by_payment_methods_query']->pluck('title'))
+                    ->values($clients_by_payment_methods_q['clients_by_payment_methods_query']->pluck('num'))
                      ->width(1200)
                      ->height(700)
                     ->responsive(false);
@@ -131,18 +271,19 @@ public  function getServiceCategoryQuery()
                     ->join('service_categories','category_id','=','service_categories.id')
                     ->groupBy('category_id','service_categories.title')
                     ->get();
-
-        return($service_by_categories_query);
+        $count=$service_by_categories_query->count();
+        return compact('service_by_categories_query','count');
 
 }
 public  function getServiceCategoryChart()
 {
-    $service_by_categories_query =StatisticsController::getServiceCategoryQuery();
+    $service_by_categories_q=StatisticsController::getServiceCategoryQuery();
     $service_by_categories = Charts::create('pie', 'highcharts')
                     ->title('service_by_categories')
+                    ->colors(StatisticsController::getcolors($service_by_categories_q['count']))
                     ->elementLabel('service_by_categories')
-                    ->labels($service_by_categories_query->pluck('title'))
-                    ->values($service_by_categories_query->pluck('num'))
+                    ->labels($service_by_categories_q['service_by_categories_query']->pluck('title'))
+                    ->values($service_by_categories_q['service_by_categories_query']->pluck('num'))
                      ->width(1200)
                      ->height(700)
                     ->responsive(false);
@@ -186,4 +327,5 @@ public function getCientChartWithTime()
                     ->groupByYear(5);
 return($client_time);
 }
+
 }
